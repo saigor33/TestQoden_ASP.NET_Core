532e85630e918cccb4d38d3ccd64d1e40c5a7f35
saigor33 <saigor33@yandex.ru>
2019-10-10 03:25:16 +0300

    Добавьте файлы проекта.

---
+++ WebApp/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp
+{
+    //+ TODO 5: unauthorized users should receive 401 status code
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ValueTask<Account> Get()
+        {
+            /// _accountService.
+            long externalId;
+            if(long.TryParse(Request.Cookies["externalId"], out externalId))
+            {
+                return _accountService.LoadOrCreateAsync(externalId); //null /*+ TODO 3: Get user id from cookie */
+            }
+            else
+            {
+                Response.StatusCode = 401;
+                return _accountService.LoadOrCreateAsync(null);
+            }
+        }
+
+        //+TODO 6: Endpoint should works only for users with "Admin" Role
+        [Authorize(Roles = "Admin")]
+        [HttpGet("{id}")]
+        public Account GetByInternalId([FromRoute] int id)
+        {
+            return _accountService.GetFromCache(id);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost("counter")]
+        public async Task UpdateAccount()
+        {
+            //Update account in cache, don't bother saving to DB, this is not an objective of this task.
+            var account = await Get();
+            account.Counter++;
+        }
+    }
+}
---
+++ WebApp/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using System.Web;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApp
+{
+    [Route("api")]
+    public class LoginController : Controller
+    {
+        private readonly IAccountDatabase _db;
+
+        public LoginController(IAccountDatabase db)
+        {
+            _db = db;
+        }
+
+        [HttpPost("sign-in")]
+        [HttpGet]
+        public async Task Login(string userName)
+        {
+            var account = await _db.FindByUserNameAsync(userName);
+            if (account != null)
+            {
+                //TODO 1: Generate auth cookie for user 'userName' with external id
+                Response.Cookies.Append("externalId", account.InternalId.ToString());
+            }
+            else
+            {
+                //TODO 2: return 404 if user not found
+                HttpContext.Response.StatusCode = 404;
+
+
+            }
+        }
+    }
+}
